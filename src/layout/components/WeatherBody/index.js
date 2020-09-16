89c7050c7def00e1115dcae9ecb15518174053f8
nandap4790 <nandap4790@gmail.com>
2020-09-16 14:59:07 +0530

    fix: add prettier and husky

--- src/layout/components/WeatherBody/index.js
+++ src/layout/components/WeatherBody/index.js
@@ -4,42 +4,47 @@ import { object } from "prop-types";
 import LocationDetailsCard from "../../../components/LocationDetailsCard";
 import SingleDayCard from "../../../components/SingleDayCard";
 
-const WeatherBody = ({cityData}) => {
+const WeatherBody = ({ cityData }) => {
   // this gives an object with dates as keys
   const cloneCityData = [...cityData.list];
   cloneCityData.forEach((item) => {
-    item.date = new Date(item.dt * 1000).toLocaleString("en-US", {day: "numeric", timeZone: "Asia/Calcutta"});
+    item.date = new Date(item.dt * 1000).toLocaleString("en-US", {
+      day: "numeric",
+      timeZone: "Asia/Calcutta",
+    });
   });
 
-  let group = cloneCityData && cloneCityData.reduce((acc, currentVal) => {
-    acc[currentVal.date] = [...acc[currentVal.date] || [], currentVal];
-    return acc;
-   }, {});
+  let group =
+    cloneCityData &&
+    cloneCityData.reduce((acc, currentVal) => {
+      acc[currentVal.date] = [...(acc[currentVal.date] || []), currentVal];
+      return acc;
+    }, {});
 
   const cityDetails = cityData.city;
-  
+
   const renderSingleDayCard = () => {
     let list = [];
-    for(let item in group){
+    for (let item in group) {
       const dateObj = group[item];
-      list.push(dateObj)
+      list.push(dateObj);
     }
 
     return list.map((item, index) => {
-      return <SingleDayCard singleDayData={item} cardIndex={index} />
+      return <SingleDayCard singleDayData={item} cardIndex={index} />;
     });
-  }
+  };
 
   return (
     <>
       <LocationDetailsCard city={cityDetails} />
       {renderSingleDayCard()}
     </>
-  )
-}
+  );
+};
 
 WeatherBody.propTypes = {
-  cityData: object
-}
+  cityData: object,
+};
 
 export default WeatherBody;
